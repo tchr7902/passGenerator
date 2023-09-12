23c5706a49cc250e14c85dd4a72f32c3816598c1
Trevor Christensen <trevorchristensen3405@gmail.com>
2023-09-11 21:06:06 -0600

    changed layout of code, finished randomizer for pass gen, and added finishing touches

--- Develop/script.js
+++ Develop/script.js
@@ -1,63 +1,59 @@
-const chars = [];
+var chars = [];
 var includeUpper;
 var includeLower;
 var includeNumbers;
 var includeSpecial;
+var passLength;
 
 function generatePassword() {
-  // password length
-  password.length = prompt("Welcome! How many characters would you like your password to be?");
-  if (password.length >= 7 && password.length <= 129) {
-  } else if (password.length < "8" && password.length > "128") {
-    alert("Password length must be between 8 and 128 characters!");
-    return;
+  // Initialize an empty password
+  var password = '';
+
+  // Password length
+  passLength = prompt("Welcome! How many characters would you like your password to be?");
+  passLength = parseInt(passLength);
+
+  if (passLength >= 8 && passLength <= 128) {
+    
+    // Uppercase
+    includeUpper = prompt("Would you like to include uppercase letters? Y/N");
+    if (includeUpper.toUpperCase() === "Y") {
+      chars.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    }
+
+    // Lowercase
+    includeLower = prompt("Would you like to include lowercase letters? Y/N");
+    if (includeLower.toUpperCase() === "Y") {
+      chars.push("abcdefghijklmnopqrstuvwxyz");
+    }
+
+    // Numbers
+    includeNumbers = prompt("Would you like to include numbers? Y/N");
+    if (includeNumbers.toUpperCase() === "Y") {
+      chars.push("0123456789");
+    }
+
+    // Special characters
+    includeSpecial = prompt("Would you like to include special characters? Y/N");
+    if (includeSpecial.toUpperCase() === "Y") {
+      chars.push("!@#$%^&*()-_+=<>?");
+    }
+
+    // Concatenate all selected character sets
+    var selectedChars = chars.join("");
+
+    // Generate the password
+    for (let i = 0; i < passLength; i++) {
+      const randomIndex = Math.floor(Math.random() * selectedChars.length);
+      password += selectedChars.charAt(randomIndex);
+    }
+
+    return password;
   } else {
-     return;
+    alert("Password length must be between 8 and 128 characters!");
+    return generatePassword(); // Restart the process if the length is invalid
   }
-
-  // uppercase
-  includeUpper = prompt("Would you like to include uppercase letters? Y/N");
-  if (includeUpper == "Y") {
-    chars.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    } else if (includeUpper == "N") {
-      } else {
-        alert("Invalid Input!")
-        return;
-  }
-
-  // lowercase
-  includeLower = prompt("Would you like to include lowercase letters? Y/N");
-  if (includeLower == "Y") {
-    chars.push("abcdefghijklmnopqrstuvwxyz")
-    } else if (includeLower == "N") {
-      } else {
-        alert("Invalid Input!")
-        return;
-  }
-
-  // numbers
-  includeNumbers = prompt("Would you like to include numbers? Y/N");
-  if (includeNumbers == "Y") {
-    chars.push("1234567890")
-    } else if (includeNumbers == "N") {
-      } else {
-        alert("Invalid Input!")
-        return;
-  }
-
-  // special
-  includeSpecial = prompt("Would you like to include lowercase special characters? Y/N");
-  if (includeSpecial == "Y") {
-    chars.push("!#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
-    } else if (includeSpecial == "N") {
-      } else {
-        alert("Invalid Input!")
-        return;
-  }
-
-  // making password
 }
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -66,7 +62,6 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
